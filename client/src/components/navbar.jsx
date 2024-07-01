4e61f248c8598112b29df657a4695b6a8566c3b3
Nkchrs <nkchrs@hotmail.fr>
2024-07-01 09:49:12 +0200

    correction

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -5,6 +5,7 @@ import panier from '../assets/panier.png';
 import Formco from "./formco";
 
 
+
 const Navbar = () => {
      const [seen, setSeen] = useState(false)
 
@@ -16,7 +17,8 @@ const Navbar = () => {
             <div className="flex justify-end bg-[#CED6C1]">
                 <ul className="flex">
                     <li className="p-4"><img src={panier} className="lmj-panier"/>Panier</li>
-                    <li className="p-4 "><img src={compte} className="lmj-compte"/><button onClick={togglePop}>Compte</button>
+                    <li className="p-4"><img src={compte} className="lmj-compte"/><button onClick={togglePop}>Compte</button>
+                    {seen ? <Formco toggle={togglePop} /> : null}</li>
                 </ul>
             </div>
         </div>
