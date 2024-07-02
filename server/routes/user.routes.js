420cb176ab586ce2c47ea922c3803a2991c50b4e
Jessica <synowski.jessica@hotmail.fr>
2024-07-02 15:53:34 +0200

    gestion de la session

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,10 +1,12 @@
 // server/routes/user.routes.js
 
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const UserController = require('../controllers/user.controller');
+const UserController = require("../controllers/user.controller");
 
 //the route for vreating a new user
-router.post('/user', UserController.createUser);
-router.get('/user/:id',UserController.getUserById)
+router.post("/user", UserController.createUser);
+//router.get("/user/:email", UserController.getUserByEmail);
+//router.get("/user/:id", UserController.getUserById);
+router.post("/user/connect", UserController.getUser);
 module.exports = router;
