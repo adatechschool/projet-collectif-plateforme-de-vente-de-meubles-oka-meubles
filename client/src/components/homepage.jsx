a38d31f47d93ad7812d493265634dd19610770b0
ileana bolas <ileana.bolas.16@gmail.com>
2024-07-03 16:40:10 +0200

    changement titre homepage

--- client/src/components/homepage.jsx
+++ client/src/components/homepage.jsx
@@ -5,23 +5,22 @@ import Footer from "./footer";
 import { ReactTyped } from 'react-typed';
 import background from "../assets/background.jpg";
 
+
 const Homepage = () => {
     return (
     <>
     <div className="h-screen bg-center bg-cover  bg-no-repeat "style={{backgroundImage: `url(${background})`}}>
-        <ReactTyped
-            strings={[
-                "OKA Meubles",
-            ]}
-            typeSpeed={80}
-            backSpeed={50}
-            loop
-        >
-        </ReactTyped>
-    
-        <h1>Ce qu'on fait :</h1>
-        <p>ici un paragraphe d'introduction</p>
-    
+        <div className="text-8xl pt-20 pl-10">
+            <ReactTyped
+                strings={[
+                    "OKA Meubles",
+                ]}
+                typeSpeed={80}
+                backSpeed={50}
+                loop
+            >
+            </ReactTyped>
+        </div>
     </div>
     </>
     )
